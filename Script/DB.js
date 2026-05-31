5b0d85858ea070616d3f82bd047ce233536e6e24
Bruno Araujo <brunokiti89@gmail.com>
2026-05-31 13:35:37 -0300

    Finalizacion de Productos

--- Script/DB.js
+++ Script/DB.js
@@ -2,37 +2,43 @@ export const Productos=[
     {
         Producto: "remera",
         nombre: "Remera de hombre de la Casa Martell",
-        img: "../src/Productos/remera_hombre",
+        img: "../src/Productos/remera_hombre.png",
+        descripcion: "Remera de algodon para hombres de alta calidad, los talles se consultan por Email",
         precio: 15000
     },
     {
         Producto: "remera",
         nombre: "Remera de mujer de la Casa Martell",
-        img: "../src/Productos/remera_mujer",
+        img: "../src/Productos/remera_mujer.png",
+        descripcion: "Remera de algodon para mujeres de alta calidad, los talles se consultan por Email",
         precio: 20000
     },
     {
         Producto: "llavero",
-        nombre: "llavero de la Casa Martell",
-        img: "../src/Productos/llavero",
+        nombre: "Llavero de la Casa Martell",
+        img: "../src/Productos/llavero.png",
+        descripcion: "LLavero personalizado de Game of Thrones, diseñado para la casa Martell",
         precio: 8000
     },
     {
         Producto: "cuadro",
-        nombre: "cuadro de la Casa Martell",
-        img: "../src/Productos/cuadro",
+        nombre: "Cuadro de la Casa Martell",
+        img: "../src/Productos/cuadro.png",
+        descripcion: "Cuadro de la casa Martell se Game of Thrones, sus caracteristicas y tamaños se consultan por Email",
         precio: 50000
     },
     {
         Producto: "peluche",
-        nombre: "peluche de Oberyn Martell",
-        img: "../src/Productos/peluche",
+        nombre: "Peluche de Oberyn Martell",
+        img: "../src/Productos/peluche.png",
+        descripcion: "Peluche de Oberyn Martell de la serie Game of Thrones, hecho de algodon y poliester",
         precio: 35000
     },
     {
         Producto: "taza",
         nombre: "Taza de la Casa Martell",
-        img: "../src/Productos/taza",
+        img: "../src/Productos/taza.png",
+        descripcion: "Taza de ceramica con el logo de la casa Martell de la serie Game of Thrones",
         precio: 12000
     }
 ]
--- Script/main.js
+++ Script/main.js
@@ -7,14 +7,28 @@ function Tarjetas(Lista) {
         let nuevoDiv = document.createElement("div")
         nuevoDiv.classList.add("Tarjeta")
         nuevoDiv.innerHTML = `
-            <img src="${i.img}" alt="Imagen">
-            <div>
-                <p>${i.nombre}</p>
-                <p class="precio">$${i.precio}</p>
-            </div>
-            <button type="button" class="btn btn-outline-light" onclick="MostrarCarro()">Agregar Al Carro</button>
+            <img src="${i.img}" alt="Productos">
+                <h3 style="font-weight:bold;">${i.nombre}</h3>
+                <p>${i.descripcion}</p>
+                <h4 style="color: green; font-size:30px; text-shadow: 2px 2px 4px #468432;" class="precio">$${i.precio}</h4>
+            <button class="btn btn-warning" type="button" onclick="MostrarCarro()">Agregar Al Carro</button>
         `
         Contenedor.appendChild(nuevoDiv)
     });
 }
-Tarjetas(Productos)
+Tarjetas(Productos);
+
+let filtro = document.querySelector("#filtro")
+let Seleccionar = document.querySelector(".Tarjetas")
+
+
+filtro.addEventListener("keyup", function () {
+
+    let filtrar = Productos.filter(i => i.nombre.toLowerCase().includes(filtro.value.toLowerCase().trim()))
+    if (filtrar.length > 0) {
+        Tarjetas(filtrar)
+    }
+    else {
+        Seleccionar.innerHTML = "<h2 '>Producto no encontrado</h2>"
+    }
+})
